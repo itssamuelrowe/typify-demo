65c1a0b1ba1f118cf62c2a0c2e52693f8dd88501
Samuel Rowe <samuelrowe1999@gmail.com>
2019-09-08 18:22:49 +0530

    Built for GitHub Pages.

---
+++ precache-manifest.d170451c1728f95bd02c878fc7095f9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b57f6bea08bafc01ca510b0b953ec4",
+    "url": "/typify-demo/index.html"
+  },
+  {
+    "revision": "469a06ee2ccdcf1fcd39",
+    "url": "/typify-demo/static/css/main.ccca1fb6.chunk.css"
+  },
+  {
+    "revision": "e0f7aa35996cd16bbd4e",
+    "url": "/typify-demo/static/js/2.4ee962de.chunk.js"
+  },
+  {
+    "revision": "469a06ee2ccdcf1fcd39",
+    "url": "/typify-demo/static/js/main.1cf7a53a.chunk.js"
+  },
+  {
+    "revision": "4d4fe95a933c897ce56b",
+    "url": "/typify-demo/static/js/runtime~main.06038825.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.718c27b3a9783b79741fb85cd50aaea1.js"
+  "/typify-demo/precache-manifest.d170451c1728f95bd02c878fc7095f9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/typify-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
